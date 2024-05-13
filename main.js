f9cc06a4293e5fb35aa6ee5101ef727c392b170e
augustrayziss <augustrayziss@gmail.com>
2024-05-13 21:26:36 +0300

    backgroud setup

---
+++ main.js
@@ -0,0 +1,12 @@
+const form = document.getElementById('signup-form');
+const inputs = form.querySelectorAll('input');
+
+inputs.forEach(input => {
+  input.addEventListener('input', () => {
+    const stars = document.querySelector('.starry-background');
+    stars.style.animation = 'none';
+    setTimeout(() => {
+      stars.style.animation = 'starry 10s infinite alternate';
+    }, 100);
+  });
+});
